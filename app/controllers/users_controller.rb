08a29b8351c52d8c1d8393ad20de341b9c2ed2b1
MatyasBalcar <matyasbalcar0@gmail.com>
2023-07-26 11:18:44 +0200

    added sign in as feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
   def index
   end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:admin,:encrypted_password)
+  end
+  def create
+    @user = User.new(user_params)
+  end
 end
